8e87d9c4cdab97936942112d41d140027eada048
Artem Vorotnikov <artem@vorotnikov.me>
2020-11-27 08:11:45 +0300

    Port to thiserror

--- src/error.rs
+++ src/error.rs
@@ -6,48 +6,42 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use failure_derive::Fail;
 use std::result;
+use thiserror::Error;
 
 /// A result with a levin error.
 pub type Result<T> = result::Result<T, Error>;
 
 /// A levin error.
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Error {
     /// An error when reading `BucketHead`.
-    #[fail(display = "couldn't parse bucket head: {}", _0)]
-    BucketHead(#[cause] BucketHeadError),
+    #[error("couldn't parse bucket head: {0}")]
+    BucketHead(#[from] BucketHeadError),
 
     /// The command is invalid.
-    #[fail(display = "the bucket command id ({}) is invalid", _0)]
+    #[error("the bucket command id ({0}) is invalid")]
     InvalidCommandId(u32),
 }
 
-impl From<BucketHeadError> for Error {
-    fn from(e: BucketHeadError) -> Error {
-        Error::BucketHead(e)
-    }
-}
-
 /// An error returned when the data of `BucketHead` is invalid.
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum BucketHeadError {
     /// The version isn't supported.
-    #[fail(display = "invalid levin protocol version (provided value: {})", _0)]
+    #[error("invalid levin protocol version (provided value: {0})")]
     InvalidProtocolVersion(u32),
 
     /// Invalid signature
-    #[fail(display = "invalid bucket signature (bad signature: {:08X})", _0)]
+    #[error("invalid bucket signature (bad signature: {:08X})", .0)]
     InvalidSignature(u64),
 
     /// An error code was returned.
-    #[fail(display = "the bucket has an error number: {}", _0)]
+    #[error("the bucket has an error number: {0}")]
     ReturnCode(i32),
 
     /// Packet too big. The maximum size of a levin bucket is [this][1]
     ///
     /// [1]: /bucket/constant.LEVIN_DEFAULT_MAX_PACKET_SIZE.html
-    #[fail(display = "the bucket size is too big ({} bytes)", _0)]
+    #[error("the bucket size is too big ({0} bytes)")]
     TooBig(u64),
 }
